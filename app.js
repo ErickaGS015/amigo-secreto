0358ad40a510435aedbab045ca8fa7c0d6da6a73
ErickaGS015 <ericka.g.itt@gmail.com>
2025-08-21 23:14:10 -0600

    Actualiza funcion de agregar

--- app.js
+++ app.js
@@ -11,9 +11,17 @@ function agregarAmigo()
     let lblAmigo = document.getElementById('amigo').value;
     if(lblAmigo != "")
     {
-        amigos.push(lblAmigo);
-        console.log(amigos);
-        limpiarLabels();
+        if(!amigos.includes(lblAmigo))
+        {
+            amigos.push(lblAmigo);
+            limpiarLabels();
+        }
+        else
+        {
+            alert("Nombre repetido");
+            limpiarLabels();
+        }        
+        //console.log(amigos);                
     }
     else
     {
@@ -26,20 +34,8 @@ function limpiarLabels()
     document.querySelector('#amigo').value="";
 }
 /*
-Descripción
-Editar
-Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
-
 Tareas específicas:
-
-Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
-
 Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
-
-Iterar sobre el arreglo: 
-    Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
-
-Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
 */
 
 function actualizarLista()
@@ -52,7 +48,19 @@ function actualizarLista()
         //console.log(amigos[i]);
         let li = document.createElement("li");
         li.textContent = amigos[i];
+
         lista.appendChild(li);
     }
 }
 
+function verificarDuplicidad()
+{
+    for(let i=0; i<amigos.length; i++)
+    {
+        if(amigos[i]==)
+        {
+
+        }
+    }
+    
+}
